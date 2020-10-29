5126b1375bc7ae48ceff83a468ac18f5578dc0af
Halimove01 <halim.dakir@gmail.com>
2020-10-29 16:10:08 +0100

    create a class wich fill in cells

--- src/main/java/org/openjfx/FillCells.java
+++ src/main/java/org/openjfx/FillCells.java
@@ -1,8 +1,24 @@
 package org.openjfx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FillCells {
+    private final static List<Cell> cellList = new ArrayList<>();
 
+    static {
+        int[][] tab2DInt ={{3,2},{4,2},{5,2},{6,5},{5,6},{6,6}};
+        for (int[] ints : tab2DInt) {
+            int[] newArray = new int[2];
+            System.arraycopy(ints, 0, newArray, 0, 2);
+            cellList.add(new Cell(newArray[0], newArray[1]));
+        }
 
-    public void insertAliveCells(Simulator simulator) {
+    }
+
+    public void insertAliveCells(Simulator simulator){
+        for (Cell cell : cellList) {
+            simulator.setAlive(cell.getX(), cell.getY());
+        }
     }
 }
