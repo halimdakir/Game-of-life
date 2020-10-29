ec09ef10cbc934db68b492f48a5d16abfcd60f13
Halimove01 <halim.dakir@gmail.com>
2020-10-29 16:13:19 +0100

    set up the start method

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -13,8 +13,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-
+        View view = new View();
+        Scene scene = new Scene(view, 400, 438);
+        stage.setScene(scene);
         stage.show();
+        view.drawOnCanvas();
     }
 
     public static void main(String[] args) {
