3c8a60ae46d2258cac2fe695f7802dbd2bdc1b46
Halimove01 <halim.dakir@gmail.com>
2020-10-29 15:59:34 +0100

    add step to next generations method and game rules

--- src/main/java/org/openjfx/Simulator.java
+++ src/main/java/org/openjfx/Simulator.java
@@ -50,4 +50,57 @@ public class Simulator {
         return counter;
     }
 
+    public void nextGenerationStep() {
+        int[][] temporaryBoard = new int[width][height];
+        checkAllCellsToApplyTheRules(height, width, temporaryBoard);
+        this.board = temporaryBoard;
+    }
+
+    public void checkAllCellsToApplyTheRules(int height, int width, int[][] temporaryBoard){
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                rulesGameOfLife(x, y, temporaryBoard);
+            }
+        }
+    }
+
+    public void rulesGameOfLife(int x, int y, int[][] temporaryBoard){
+        if (getCellState(x, y) == 1) {
+
+            temporaryBoard[x][y] = stateIsAliveRules(countAliveNeighbours(x, y), temporaryBoard, x, y);
+
+        } else {
+
+            temporaryBoard[x][y] = stateIsDeadRules(countAliveNeighbours(x, y), temporaryBoard, x, y);
+
+        }
+
+    }
+
+    private int casesWhenStateIsAlive(int a) {
+        int i = 0;
+        if (a  > 3 || a  < 2) i = 1;    //Any live cell [ with more than three or fewer than two live neighbors] dies.
+        if (a == 2 || a == 3) i = 2;    //Any live cell [ with two or three live neighbors ] lives.
+        return i;
+    }
+
+    public int stateIsAliveRules(int neighboursAreAlive, int[][] temporaryBoard, int x, int y){
+        switch (casesWhenStateIsAlive(neighboursAreAlive)) {
+            case 1:
+                temporaryBoard[x][y]= 0;
+                break;
+            case 2:
+                temporaryBoard[x][y] = 1;
+                break;
+        }
+        return temporaryBoard[x][y];
+    }
+
+    public int stateIsDeadRules(int neighboursAreAlive, int[][] temporaryBoard, int x, int y){
+        if (neighboursAreAlive == 3)
+            temporaryBoard[x][y] = 1;  //Any dead cell with exactly three live neighbors becomes a live cell
+
+        return temporaryBoard[x][y];
+    }
+
 }
